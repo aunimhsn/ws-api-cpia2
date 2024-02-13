885bcf391ff037e9c8ef0a5af77d6e6c1af47cb2
Aunim HASSAN <aunim.hassan@gmail.com>
2024-02-13 10:50:13 +0100

    feat: inline avatar with css

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,31 @@
-async function getUsers() {
-    const response = await fetch("https://reqres.in/api/users")
+async function getUsers(page = 1) {
+    const response = await fetch(`https://reqres.in/api/users?page=${page}`)
     const users = await response.json()
     if (response.status == 200) return users
     else throw Error('API error has occured')
 }
 
-let btnGet = document.getElementById("btn-get")
+let btnGet = document.getElementById('btn-get')
 btnGet.addEventListener('click', () => {
-    getUsers().then(users => {
-        let getResult = document.getElementById("get-result")
+    getUsers(2).then(users => {
+        let getResult = document.getElementById('get-result')
 
         for (const user of users['data']) {
-            const fullname = document.createElement("p");
-            getResult.appendChild(fullname)
+            const container = document.createElement('div')
+            getResult.appendChild(container)
+
+            const fullname = document.createElement('p')
+            container.appendChild(fullname)
             fullname.innerHTML = `${user['first_name']} - ${user['last_name']}`
+
+            const email = document.createElement('p')
+            container.appendChild(email)
+            email.innerHTML = user['email']
+            
+            const avatar = document.createElement('img')
+            container.appendChild(avatar)
+            avatar.setAttribute('src', user['avatar'])
+            avatar.setAttribute('alt', 'Profil / Avatar');
         }
     })
 })
